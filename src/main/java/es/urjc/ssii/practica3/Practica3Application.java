19011c1de571cfa4b2dd52c270412fa347dfe2f5
mikamp116 <mikamardanyan@gmail.com>
2020-04-20 13:33:02 +0200

    Initial commit

---
+++ src/main/java/es/urjc/ssii/practica3/Practica3Application.java
@@ -0,0 +1,13 @@
+package es.urjc.ssii.practica3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica3Application.class, args);
+    }
+
+}
